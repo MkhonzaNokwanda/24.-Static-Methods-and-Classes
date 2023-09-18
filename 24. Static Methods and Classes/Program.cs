5781030ba894bb8cb6179f0b3a08c3ccebc2f28b
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-18 14:31:46 +0200

    Add project files.

---
+++ 24. Static Methods and Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Static_Methods_and_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(144));
+            SayHi salute=new SayHi();
+            salute.sayHello("Nono","Mudau");
+
+            //freeze
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 24. Static Methods and Classes/SayHi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Static_Methods_and_Classes
+{
+    internal class SayHi
+    {
+        public void sayHello(string name, string surname) 
+        {
+            Console.WriteLine("Hello! " + name + " " + surname);
+
+           
+        }
+    }
+}
